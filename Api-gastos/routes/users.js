207b2d8ee1b9f6ed468b7873ca37b66c09fae402
Rubén Gianelli <rubengianelli8@gmail.com>
2021-07-29 11:18:43 -0300

    api- user login section

--- Api-gastos/routes/users.js
+++ Api-gastos/routes/users.js
@@ -19,11 +19,8 @@ route.post('/register',(req, res) => {
     req.getConnection((err, conn) =>{
         if(err) return res.send(err);
         conn.query('use db_billetera');
-        //
         conn.query(`SELECT * FROM usuarios WHERE correo= '${req.body.correo}'`, (err, rows) =>{
             if(err) return res.send(err);
-            let users = [];
-            users = rows;
             if(rows.length > 0){
                 res.send('the user already exists');
             }else{
@@ -47,7 +44,19 @@ route.post('/register',(req, res) => {
 route.post('/login', (req, res) => {
     req.getConnection((err, conn) =>{
         if(err) return res.send(err);
-
+        conn.query('use db_billetera');
+        conn.query(`SELECT * FROM usuarios WHERE correo= '${req.body.correo}'`, (err, rows) => {
+            if(err) return res.send(err);
+            if(rows.length > 0){
+                if(rows[0].password === req.body.password){
+                    res.json(rows[0])
+                }else{
+                    res.send('la contraseña ingresada es incorrecta');
+                }
+            }else{
+                res.status(400).send('The user entered does not exist or the password is not valid');
+            }
+        })
 
     })
 })
