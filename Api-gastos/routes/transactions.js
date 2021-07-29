31c41ca07c4d76c4ae6b3204e919fc7a1308ff38
Rubén Gianelli <rubengianelli8@gmail.com>
2021-07-29 18:12:01 -0300

    added token system

--- Api-gastos/routes/transactions.js
+++ Api-gastos/routes/transactions.js
@@ -6,14 +6,21 @@ const id_usuario = require('../userLoged');//id user loged
 //routes------------
 //add transaction
 routeTransaction.post('/', (req, res) =>{
-    if(!req.body.monto || !req.body.concepto || !req.body.tipo || !req.body.fecha || !req.body.id_usuario){
+    if(!req.body.monto || !req.body.concepto || !req.body.tipo || !req.body.fecha || !req.body.id_usuario || !req.body.token){
         return res.status(400).send('a parameter is missing');
     }
     req.getConnection((err, conn) => {
         if(err) return res.send(err);
         conn.query('use db_billetera');
+        conn.query(`SELECT token FROM usuarios WHERE id = ${req.body.id_usuario}`, (err, rows) =>{
+            if(rows[0].token != req.body.token){
+                return res.send('the token is invalid')
+            }
+        })
+        const {monto, concepto, tipo, fecha, id_usuario} = req.body;
+        const newBody = {monto, concepto, tipo, fecha, id_usuario};
         //add transaction
-        conn.query('INSERT INTO operaciones set ?',[req.body] ,(err, rows) => {
+        conn.query('INSERT INTO operaciones set ?',[newBody] ,(err, rows) => {
             if(err) return res.send(err)
             res.send('the transaction has been added');
         });
@@ -22,14 +29,21 @@ routeTransaction.post('/', (req, res) =>{
 
 //update transaction
 routeTransaction.put('/:id', (req, res) =>{
-    if(!req.body.monto || !req.body.concepto || !req.body.tipo || !req.body.fecha || !req.body.id_usuario){
+    if(!req.body.monto || !req.body.concepto || !req.body.tipo || !req.body.fecha || !req.body.id_usuario || !req.body.token){
         return res.status(400).send('a parameter is missing');
     }
     req.getConnection((err, conn) => {
         if(err) return res.send(err);
         conn.query('use db_billetera');
+        conn.query(`SELECT token FROM usuarios WHERE id = ${req.body.id_usuario}`, (err, rows) =>{
+            if(rows[0].token != req.body.token){
+                return res.send('the token is invalid')
+            }
+        })
+        const {monto, concepto, tipo, fecha, id_usuario} = req.body;
+        const newBody = {monto, concepto, tipo, fecha, id_usuario};
         //add transaction
-        conn.query('UPDATE operaciones set ? WHERE id = ?',[req.body, req.params.id] ,(err, rows) => {
+        conn.query('UPDATE operaciones set ? WHERE id = ?',[newBody, req.params.id] ,(err, rows) => {
             if(err) return res.send(err)
             res.send('the transaction has been updated');
         });
@@ -80,7 +94,12 @@ routeTransaction.delete('/:id', (req, res) =>{
     req.getConnection((err, conn) => {
         if(err) return res.send(err);
         conn.query('use db_billetera');
-        //add transaction
+        conn.query(`SELECT token FROM usuarios WHERE id = ${req.body.id_usuario}`, (err, rows) =>{
+            if(rows[0].token != req.body.token){
+                return res.send('the token is invalid')
+            }
+        })
+        //delete transaction
         conn.query('DELETE FROM operaciones WHERE id = ?',[req.params.id] ,(err) => {
             if(err) return res.send(err)
             res.send(`the transaction with id= ${req.params.id}has been deleted`);
--- Api-gastos/routes/users.js
+++ Api-gastos/routes/users.js
@@ -22,10 +22,16 @@ route.post('/register',(req, res) => {
                     return;
                 }
                 //register user
-                conn.query('INSERT INTO usuarios set ?',[req.body] ,(err, rows) => {
+                let token = tokenGenerator() + tokenGenerator();
+                const user={
+                    ...req.body, token
+                }
+                conn.query('INSERT INTO usuarios set ?',[user] ,(err, rows) => {
                     if(err) return res.send(err)
                     res.send('the user has been registered');
+                    
                 });
+                
             }
         });
     });
@@ -41,7 +47,7 @@ route.post('/login', (req, res) => {
             if(rows.length > 0){
                 //login success
                 if(rows[0].password === req.body.password){
-                    res.json(rows[0])
+                    res.json(rows[0]);
                 }else{
                     res.send('the password entered is incorrect');
                 }
@@ -66,5 +72,9 @@ const validateEmail = (email) =>{
     return (schema.validate({correo: email}));
 }
 
+//GENERATE TOKEN
+const tokenGenerator = () =>{
+    return Math.random().toString(36).substr(2);
+}
 module.exports = route;
 
